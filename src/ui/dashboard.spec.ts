e63eacef43c5e2871e4b3e8b17d7b9ae00cbc797
Byambajav <byambajav.munkhbayar@erin.systems>
2023-06-06 13:02:02 +0800

    fix: style

--- src/ui/dashboard.spec.ts
+++ src/ui/dashboard.spec.ts
@@ -1,20 +1,20 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 const authFile = "playwright/.auth/user.json";
 
-test("authenticate", async ({ page }) => {
-  await page.goto("https://test-accounting.erin.systems/");
-  await page.type("#username", "byambaa");
-  await page.type("#password", "Secret123");
-  await page.click("#kc-form-buttons > input");
-  await page.waitForURL("https://test-accounting.erin.systems/#/dashboard");
-  const parentDivElement = await page.waitForSelector("#accounting");
-  await parentDivElement?.click();
-
-  await page.waitForURL("https://test-accounting.erin.systems/#/accounting");
-  const frame = page.frameLocator("#accounting");
-  expect(frame).toBeTruthy();
-  expect(frame.locator("div")).toHaveId("root");
-
+test("dashboard", async ({ page }) => {
+  await page.goto("https://test-lms.erin.systems/#/login");
+  await page.fill('input[placeholder="Хэрэглэгчийн нэр"]', "playwright");
+  await page.fill('input[type="Нууц үг"]', "Secret123");
+  await page.click("button");
+  await page.waitForURL("https://test-lms.erin.systems/#/dashboard");
+  await page.isVisible("text=ЦАХИМ СУРГАЛТ");
+  await page.isVisible("text=Нийт суралцагчид");
+  await page.isVisible("text=90% танилцсан");
+  await page.isVisible("text=Танилцсан дундаж хувь");
+  await page.isVisible("text=Цахим сургалттай танилцсан хугацаа");
+  await page.isVisible("text=Алба хэлтэс");
+  await page.isVisible("text=Эхлэх");
+  await page.isVisible("text=Дуусгах");
   await page.context().storageState({ path: authFile });
 });
